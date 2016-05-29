1d987391097230849b80d4f4f5416c1668709a90
ZL1996 <649349296@qq.com>
2016-05-29 13:40:12 +0800

    Update double_dimensional_array_to_one_1.js

--- practices/flaten/double_dimensional_array_to_one_1.js
+++ practices/flaten/double_dimensional_array_to_one_1.js
@@ -1,8 +1,17 @@
 'use strict';
 
 function double_to_one(collection) {
+  var result = [];
+  putIn(collection[0],result);
+  for(var i=0;i<collection.length;i++){
+    for(var j=0;j<collection[i].length;j++)
+      putIn(collection[i][j],result);
+  }
+  return result;
+}
 
-  //在这里写入代码
+function putIn(a,str){
+  str.push(a);
 }
 
 module.exports = double_to_one;
